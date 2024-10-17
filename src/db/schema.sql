1b7b123b7cf7d628b9c07ed5fa75c7ab6d2341bf
ryannarnoldd <ryannarnoldd@gmail.com>
2024-10-17 11:27:26 -0400

    More messing around with seeds.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -6,14 +6,12 @@ CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
-
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL
 );
-
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
